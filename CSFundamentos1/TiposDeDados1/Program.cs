5ac9d3c19dd3cf71c8692f51fcfe3af5e4d93077
CAIQUE2SILVA <64806656+CAIQUE2SILVA@users.noreply.github.com>
2024-05-08 13:14:13 -0300

    aula 1

---
+++ CSFundamentos1/TiposDeDados1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Declarando variáveis numéricas");
+Console.WriteLine();
+
+// declarando variaveis
+
+byte valor1 = 255;
+sbyte valor2 = -127;
+int valor3 = -2147483647;
+valor3 = 100;
+uint valor4 = 2147483647;
+long valor5 = 2147483689;
+
+const int valor6 = 999;
+//valor6 = 888
+
+int var1= 8, var2= 10, var3= 11;
+
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+Console.WriteLine(valor4);
+Console.WriteLine(valor5);
+Console.WriteLine(valor6);
+Console.WriteLine(var1);
+Console.WriteLine(var2);
+Console.WriteLine(var3);
